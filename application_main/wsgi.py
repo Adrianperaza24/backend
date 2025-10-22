eab6d634528f8f36c43f0c8da6fcd617270e9708
Luis Adrian Peraza Aguirre <lperaza@enerya.com>
2025-10-22 11:14:01 -0600

    WSGI Error update

--- application_main/wsgi.py
+++ application_main/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'busoptix_datam_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application_main.settings')
 
 application = get_wsgi_application()
